d531abdb0e57de41e02e3e8be57a6c4529084c98
Ariel Betti <ariel.betti@gmail.com>
2023-03-29 01:30:54 -0300

    feat(word-component): create word ui component

---
+++ src/components/Word.tsx
@@ -0,0 +1,16 @@
+import randomColor from "randomcolor";
+import React from "react";
+
+type TWordProps = {
+  word: string;
+};
+
+export const Word = ({ word }: TWordProps) => {
+  return (
+    <p className="p-2 bg-base-300 text-base-content rounded-md shadow-md">
+      {word}
+    </p>
+  );
+};
+
+export default Word;
