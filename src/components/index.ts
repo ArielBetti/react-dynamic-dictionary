37dad3b8405bd679678a71de82d83f2feae1d527
Ariel Betti <ariel.betti@gmail.com>
2023-03-29 01:31:02 -0300

    feat: export all components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import Card from "./Card";
+import Code from "./Code";
+import Paragraph from "./Paragraph";
+import Selector from "./Selector";
+import ThemeSelector from "./ThemeSelector";
+import Word from "./Word";
+
+export { Card, Code, Paragraph, Selector, ThemeSelector, Word };
